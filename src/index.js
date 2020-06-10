8f177712b66f4c03c10aa5e74ade4a18d756b374
cosmicChimp <ian.t.marrs@gmail.com>
2020-06-10 13:47:28 -0600

    imported createStore, applyMiddleware, Provider and thunk to app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+const store = createStore(applyMiddleware(store));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
