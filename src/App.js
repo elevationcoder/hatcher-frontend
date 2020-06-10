bc593038105b590a7f19061769dc76e591d9abc9
cosmicChimp <ian.t.marrs@gmail.com>
2020-06-10 11:27:30 -0600

    added test fetch to App with successful result

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  componentDidMount() {
+    fetch("http://localhost:3001/egg_bundles/1")
+      .then((resp) => resp.json())
+      .then((data) => console.log(data.flocks));
+  }
   render() {
     return <div className="App">App</div>;
   }
