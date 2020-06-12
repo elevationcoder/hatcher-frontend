0df494e2012a0a58b2bf38b12839dd9bdaa0c1b0
cosmicChimp <ian.t.marrs@gmail.com>
2020-06-12 17:55:06 -0600

    cleaned up BundleContainer

--- src/containers/BundleContainer.js
+++ src/containers/BundleContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import NewBundleInput from "../components/NewBundleInput";
 import Bundles from "../components/Bundles";
 class BundleContainer extends Component {
-  state = {};
   render() {
     return (
       <div>
